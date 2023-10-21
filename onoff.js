8263b2b1a46eea1617ebeb5714765cf7075ba7e0
backiyadeveloper <143633955+backiyadeveloper@users.noreply.github.com>
2023-10-21 13:26:56 +0530

    on off

---
+++ onoff.js
@@ -0,0 +1,8 @@
+function on(){
+      var image=document.getElementById("onimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+      var catImgae=document.getElementById("catimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+}
+function off(){
+    var image=document.getElementById("onimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    var catImgae=document.getElementById("catimg").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+}
